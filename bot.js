c28310394367d996bab3f4b855abc968d1e1cc0c
Zolhungaj <26750497+Zolhungaj@users.noreply.github.com>
2020-05-13 19:37:48 +0200

    added more functions for eventhandling in rooms

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const {SocketWrapper, getToken, EVENTS, sleep} = require('node/amq-api')
 const fs = require('fs');
+const { KICKED_FROM_GAME } = require('./node/eventstodo');
 
 async function main() {
     const debug = true
@@ -32,6 +33,13 @@ class Room {
         this.activePlayers = {}
         this.spectators = {}
         this.playerLeftListener = socket.on(EVENTS.PLAYER_LEFT, (data) => playerLeft(data))
+        this.playerJoinedListener = socket.on(EVENTS.NEW_PLAYER, (data) => playerJoined(data))
+        this.spectatorJoinedListener = socket.on(EVENTS.NEW_SPECTATOR, (data) => spectatorJoined(data))
+        this.spectatorLeftListener = socket.on(EVENTS.SPECTATOR_LEFT, (data) => spectatorLeft(data))
+        this.spectatorChangedToPlayerListener = socket.on(EVENTS.SPECTATOR_CHANGED_TO_PLAYER, (data) => spectatorChangedToPlayer(data))
+        this.playerNameChangedListener = socket.on(EVENTS.PLAYER_NAME_CHANGE, (data) => playerNameChanged(data))
+        this.spectatorNameChangedListener = socket.on(EVENTS.SPECTATOR_NAME_CHANGE, (data) => spectatorNameChanged(data))
+        this.globalNameChangedListener = socket.on(EVENTS.ALL_PLAYER_NAME_CHANGE, (data) => globalNameChanged(data))
     }
     
     playerLeft = (data) => {
@@ -44,8 +52,122 @@ class Room {
         delete players[data.player.gamePlayerId]
     }
 
+    spectatorLeft = (data) => {
+        //data.
+        //     newHost   //string
+        //     kicked    //boolean
+        //     spectator //string
+        //
+        delete spectators[data.spectator]
+    }
+
+    playerJoined = (playerData) => {
+        //playerData.
+        //           name          //string, unique
+        //           level         //integer
+        //           gamePlayerId  //integer, position
+        //           ready         //boolean
+        //           inGame        //boolean
+        //           avatar.       
+        //                  avatar.
+        //                         active             //integer/boolean
+        //                         avatarId           //integer
+        //                         avatarName         //string
+        //                         backgroundFileName //string/filename
+        //                         characterId        //integer
+        //                         colorActive        //integer
+        //                         colorId            //integer
+        //                         colorName          //string
+        //                         editor             //null?
+        //                         optionActive       //boolean
+        //                         optionName         //string
+        //                         outfitName         //string
+        //                  background.
+        //                             avatarName     //string
+        //                             backgroundHori //string/filename
+        //                             backgroundVert //string/filename
+        //                             outfitName     //string
+        player = database.getPlayer(playerData.name)
+        if (!player) {
+            player = data
+            database.newPlayer(player)
+        }
+        if(player.banned) {
+            kick(playerData.name)
+        }else{
+            players[playerData.name] = player
+        }
+    }
+    
+    spectatorJoined = (spectator) => {
+        //spectator.
+        //          name         // string
+        //          gamePlayerId // integer but always null
+        player = database.getSpectator(spectator.name)
+        if (!player) {
+            player = {name: spectator.name, banned: false}
+            database.newSpectator(spectator.name)
+        }
+        if(player.banned) {
+            kick(player.name)
+        }else{
+            spectators[player.name] = player
+        }
+    }
+
+    spectatorChangedToPlayer = (player) => {
+        //same kind of data as playerJoined
+        delete spectators[player.name]
+        playerJoined(player)
+    }
+
+    playerNameChanged = (data) => {
+        //data.
+        //     oldName      //string
+        //     newName      //string
+        //     gamePlayerId //integer
+        const oldName = data.oldName
+        const newName = data.newName
+        database.changeName(oldName, newName)
+        if(players[oldName]) {
+            players[oldName].name = newName
+            players[newName] = players[oldName]
+            delete players[oldName]
+        }
+        if(this.activePlayers[oldName]) {
+            activePlayers[oldName].name = newName
+            activePlayers[newName] = activePlayers[oldName]
+            delete activePlayers[oldName]
+        }
+    }
+
+    specatorNameChanged = (data) => {
+        //data.
+        //     oldName //string
+        //     newName //string
+        const oldName = data.oldName
+        const newName = data.newName
+        database.changeName(oldName, newName)
+        if(spectators[oldName]) {
+            spectators[oldName].name = newName
+            spectators[newName] = spectators[oldName]
+            delete spectators[oldName]
+        }
+    }
+
+    globalNameChanged = (data) => {
+        //this is related to the all players list, and thus includes all online players
+        //data.
+        //     oldName //string
+        //     newName //string
+        const oldName = data.oldName
+        const newName = data.newName
+        database.changeName(oldName, newName)
+    }
+
     destroy = () => {
         playerLeftListener.destroy()
+        playerJoinedListener.destroy()
     }
 
     start = () => {
